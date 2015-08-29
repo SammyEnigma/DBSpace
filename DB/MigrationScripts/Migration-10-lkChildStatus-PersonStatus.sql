d5c7ac5877a96f085297487acfd3852527626811
liamehinc <sysproxy@hotmail.com>
2015-08-28 22:46:21 -0600

    migration script update

--- DB/MigrationScripts/Migration-10-lkChildStatus-PersonStatus.sql
+++ DB/MigrationScripts/Migration-10-lkChildStatus-PersonStatus.sql
@@ -1,13 +1,20 @@
 USE LCCHPPublic
 GO
 SET NOCOUNT ON;
-insert into PersonStatus (PersonStatusName,HistoricPersonStatusID)
-Select ChildStatus,ChildStatusCode from LCCHPImport..lkChildStatus
+insert into TargetStatus(StatusName,TargetType)
+Select ChildStatus,'Person' from LCCHPImport..lkChildStatus
 
 select 'comparing personstatus row count to lkchildstatus row count'
 select count(*) from PersonStatus
 select count(*) from LCCHPImport..lkChildStatus
 
 select 'comparing personstatus contents to lkchildstatus contents'
-Select HistoricPersonStatusID,PersonStatusName from PersonStatus
+Select StatusName,* from TargetStatus
+
+Recheck, Closed, Moved, Resolved, Home Visit, Complete
+update 
+select * from TargetStatus
 Select * from LCCHPImport..lkChildStatus
+
+
+
